10149a77caee73be8b0324f52df92468008f8633
jinyao <jinyao383@hotmail.com>
2018-12-14 16:02:40 +0100

    Add semaphore for i2c master.

--- flypulator_physical/src/i2c_to_esc.cpp
+++ flypulator_physical/src/i2c_to_esc.cpp
@@ -7,6 +7,7 @@
 // handle for all motors
 Motors *motors_ptr;
 float g_upper_limit = 6000;
+bool sem_i2c = true; // binary semaphore for i2c master
 
 bool flag_in_control = false;
 
@@ -17,19 +18,24 @@ void controlMsgCallback(const flypulator_common_msgs::RotorVelStamped msg)
   if (motors_ptr->getMotorsState() != Motors::MOTOR_ARMED)
     return;
 
-  if(!flag_in_control){
+  if (!flag_in_control)
+  {
     flag_in_control = true;
     ROS_WARN_STREAM("Controller ON!");
   }
 
   for (int i = 0; i < 6; ++i)
-    input[i] = msg.velocity[i]>g_upper_limit?g_upper_limit:msg.velocity[i];
+    input[i] = msg.velocity[i] > g_upper_limit ? g_upper_limit : msg.velocity[i];
 
   motors_ptr->setMotorsVel(input);
 
   ROS_INFO_STREAM("controlMsgCallback() Write Motors!");
+
+  while (!sem_i2c);
+  sem_i2c = false;
   if (motors_ptr->writeMotors() != Motors::WRITE_OK)
     printf(PRED "writeMotor error!" PRST);
+  sem_i2c = true;
 }
 
 void dynamicParamCallback(flypulator_mavros::offb_parameterConfig &config, uint32_t level)
@@ -37,7 +43,8 @@ void dynamicParamCallback(flypulator_mavros::offb_parameterConfig &config, uint3
   if (config.drone_armed)
   {
     // arm motors
-    if (motors_ptr->getMotorsState() != Motors::MOTOR_ARMED){
+    if (motors_ptr->getMotorsState() != Motors::MOTOR_ARMED)
+    {
       motors_ptr->armMotors();
       ROS_WARN_STREAM("Motor ARMED!");
     }
@@ -45,7 +52,8 @@ void dynamicParamCallback(flypulator_mavros::offb_parameterConfig &config, uint3
   else
   {
     // disarm motors
-    if (motors_ptr->getMotorsState() != Motors::MOTOR_DISARMED){
+    if (motors_ptr->getMotorsState() != Motors::MOTOR_DISARMED)
+    {
       motors_ptr->disarmMotors();
       ROS_WARN_STREAM("Motor DISARMED!");
     }
@@ -56,18 +64,18 @@ void dynamicParamCallback(flypulator_mavros::offb_parameterConfig &config, uint3
 
   g_upper_limit = config.upper_limit;
 
-  if ((!flag_in_control)&&(motors_ptr->getMotorsState() != Motors::MOTOR_DISARMED))
+  if ((!flag_in_control) && (motors_ptr->getMotorsState() != Motors::MOTOR_DISARMED))
   {
-    float input[6]={0};
-    input[0] = config.motor1_speed; 
+    float input[6] = {0};
+    input[0] = config.motor1_speed;
     input[1] = config.motor2_speed;
-    input[2] = config.motor3_speed; 
+    input[2] = config.motor3_speed;
     input[3] = config.motor4_speed;
-    input[4] = config.motor5_speed; 
-    input[5] = config.motor6_speed; 
+    input[4] = config.motor5_speed;
+    input[5] = config.motor6_speed;
 
-    for(int i=0; i<6; ++i)
-      input[i] = input[i]>g_upper_limit?g_upper_limit:input[i];
+    for (int i = 0; i < 6; ++i)
+      input[i] = input[i] > g_upper_limit ? g_upper_limit : input[i];
 
     motors_ptr->setMotorsVel(input);
     ROS_INFO_STREAM("Set Motors speed!");
@@ -94,10 +102,15 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(100);
   while (ros::ok())
   {
-    if (!flag_in_control){
+    if (!flag_in_control)
+    {
       ROS_INFO_STREAM("main() Write Motors!");
+
+      while (!sem_i2c);
+        sem_i2c = true;
       if (motors_ptr->writeMotors() != Motors::WRITE_OK)
         printf(PRED "writeMotor error!" PRST);
+      sem_i2c = false;
     }
 
     ros::spinOnce();
