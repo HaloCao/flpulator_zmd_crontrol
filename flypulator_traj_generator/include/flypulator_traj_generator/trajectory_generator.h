996b38f7a3ce7066c1145daf2c831c3ad2750d31
Chao Yao <chao.yao@tu-dresden.de>
2019-03-22 19:00:47 +0100

    [traj generator] replace self-defined struct EulerParams with standard Eigen::AngleAxis

--- flypulator_traj_generator/include/flypulator_traj_generator/trajectory_generator.h
+++ flypulator_traj_generator/include/flypulator_traj_generator/trajectory_generator.h
@@ -12,8 +12,8 @@
 // include message structs
 #include "trajectory_msgs/MultiDOFJointTrajectoryPoint.h"
 
-#include <eigen3/Eigen/Dense>  //TODO Why does <Eigen/Dense> not work??
-#include <math.h>              //sin,cos functions, M_PI
+#include <Eigen/Dense>  //matrix, geometry
+#include <math.h>       //sin,cos functions, M_PI
 
 namespace trajectory_types  // own namespace for enumeration following style guide http://wiki.ros.org/CppStyleGuide
 {
@@ -24,15 +24,6 @@ enum Type
   // add new trajectory types here
 };
 };
-
-namespace Eigen
-{
-struct EulerParams
-{
-  Eigen::Vector3f axis;
-  float angle;
-};
-};  // namespace Eigen
 
 namespace trajectory
 {
@@ -51,7 +42,18 @@ public:  // constructor takes publisher to publish message
     traj_visualization_pub_ = visu_pub;
   }
 
-  // create and send trajectory with pose estimation frequency
+  /**
+   * \brief createAndSendTrajectory Create trajectories and send it with control frequency if desired
+   * \param p_start Start position
+   * \param p_end Target position
+   * \param rpy_start Start attitude in RPY
+   * \param rpy_end Target attitude in RPY
+   * \param duration Time duration of the trajectory
+   * \param start_tracking Flag if the created trajectory will be sent
+   * \param traj_type Trajectory type
+   * \param others other params are for visualization purpose
+   * \return True if Trajectory created successfully
+   */
   bool createAndSendTrajectory(const geometry_msgs::Vector3& p_start, const geometry_msgs::Vector3& p_end,
                                const geometry_msgs::Vector3& rpy_start, const geometry_msgs::Vector3& rpy_end,
                                const float duration, const bool start_tracking, const trajectory_types::Type traj_type,
@@ -66,36 +68,67 @@ private:
   ros::Publisher trajectory_publisher_;
   // publisher for visualization purpose
   ros::Publisher traj_visualization_pub_;
-  // create trajectory message
+
+  /**
+   * \brief generateTrajectoryMessage Create trajectory messages
+   */
   trajectory_msgs::MultiDOFJointTrajectoryPoint generateTrajectoryMessage(
       const float p[6], const float p_dot[6], const float p_ddot[6], const Eigen::Quaternionf& q,
       const Eigen::Vector3f& omega, const Eigen::Vector3f& omega_dot, const ros::Duration& time_from_start);
 
-  // evaluate acceleration polynom at a given time
+  /**
+   * \brief evaluateAcceleration Evaluate the acceleration polynom at a given time
+   * \return Desired acceleration at time t
+   */
   inline float evaluateAcceleration(float a2, float a3, float a4, float a5, float t);
 
-  // evaluate velocity polynom at a given time
+  /**
+   * \brief evaluateVelocity Evaluate the velocity polynom at a given time
+   * \return Desired velocity at time t
+   */
   inline float evaluateVelocity(float a1, float a2, float a3, float a4, float a5, float t);
 
-  // evaluate position polynom at a given time
+  /**
+   * \brief evaluatePosition Evaluate the position polynom at a given time
+   * \return Desired position at time t
+   */
   inline float evaluatePosition(float a0, float a1, float a2, float a3, float a4, float a5, float t);
 
-  // calculate euler parameters
-  inline void calculateEulerParameters(Eigen::Matrix3f rotMat, Eigen::EulerParams& eulerParams);
-
-  // convert RPY-Angles to discrete cosine matrix
+  /**
+   * \brief rpyToRotMat convert RPY-Angles to rotation matrix
+   * \param roll Roll angle
+   * \param pitch Pitch angle
+   * \param yaw Yaw angle
+   * \param rotMatrix Rotation matrix converted from RPY
+   */
   inline void rpyToRotMat(float roll, float pitch, float yaw, Eigen::Matrix3f& rotMatrix);
 
-  // extract start and target position and extract corresponding euler parameters
+  /**
+   * \brief extractStartAndTargetPose extract start and target position and extract corresponding euler parameters
+   * \param p_start Input, start position
+   * \param p_end Input, target position
+   * \param rpy_start Input, start attitude in RPY
+   * \param rpy_end Input, target attitude in RPY
+   * \param pose_start[] Output, start value for position and angle of Euler parameter
+   * \param pose_end[] Output, target value for position and angle of Euler parameter
+   * \param euler_param Output, Euler paramter derived from start and end attitude
+   * \param q_start Output, start attitude
+   */
   void extractStartAndTargetPose(const geometry_msgs::Vector3& p_start, const geometry_msgs::Vector3& p_end,
                                  const geometry_msgs::Vector3& rpy_start, const geometry_msgs::Vector3& rpy_end,
-                                 float pose_start[], float pose_end[], Eigen::EulerParams& eulerParams,
+                                 float pose_start[], float pose_end[], Eigen::AngleAxisf& euler_param,
                                  Eigen::Quaternionf& q_start);
 
-  // convert euler parameters to quaternion
-  void eulerParamsToQuat(Eigen::Vector3f euler_axis, float euler_angle, Eigen::Quaternionf& q_AB);
-
-  // get angular velocity and acceleration from euler angle and its derivatives
+  /**
+   * \brief angularVelocityFromEulerParams get angular velocity and acceleration from euler angle and its derivatives
+   * \param q_IA  Quaternion
+   * \param kA  Euler axis
+   * \param the Euler angle
+   * \param dthe Derivative of Euler angle
+   * \param ddthe Second order derivative of Euler angle
+   * \param omeg Angular velocity
+   * \param omeg_dot Derivative of angular velocity
+   */
   void angularVelocityFromEulerParams(Eigen::Quaternionf q_IA, Eigen::Vector3f kA, float the, float dthe, float ddthe,
                                       Eigen::Vector3f& omeg, Eigen::Vector3f& omeg_dot);
 };
