e583f6131323211a9c26b49b9feb1a59740f4d10
Chao Yao <chao.yao@tu-dresden.de>
2019-04-09 20:07:48 +0200

    FakeSensorPlugin unfinished

--- flypulator_gazebo_plugin/include/fake_sensor_plugin.hpp
+++ flypulator_gazebo_plugin/include/fake_sensor_plugin.hpp
@@ -30,14 +30,19 @@
 
 namespace gazebo
 {
+bool write_data_2_file = true;  // save pose data to file
+std::string file_path = "/home/chao/flypulator_ws/src/flypulator/flypulator_gazebo_plugin/position_data_refstep.csv";
+
+int g_output_rate_divider =
+    10;  // output rate = 1000Hz/ouput_rate_divider // used if not specified in state_estimation_param_yaml
+unsigned size_of_queue =
+    2;  // number of messages which meas_state messages are delayed by if not specified in state_estimation_param_yaml
+
 class FakeSensorPlugin : public ModelPlugin
 {
-  /// \brief constructor
 public:
-  FakeSensorPlugin()
-  {
-  }
-  /// \brief    Destructor.
+  FakeSensorPlugin();
+
 public:
   virtual ~FakeSensorPlugin();
 
@@ -97,58 +102,18 @@ private:
 private:
   std::queue<flypulator_common_msgs::UavStateStamped> queue;
 
-  bool write_data_2_file = false;  // save pose data to file
-  std::string file_path = "/home/jan/flypulator_ws/src/flypulator/flypulator_gazebo_plugin/"
-                          "position_data_refstep.csv";
-
-  int g_output_rate_divider =
-      10;  // output rate = 1000Hz/ouput_rate_divider // used if not specified in state_estimation_param_yaml
-  unsigned size_of_queue =
-      2;  // number of messages which meas_state messages are delayed by if not specified in state_estimation_param_yaml
-
-  // initial values for noise generators if no noise specified in state_estimation_param.yaml
-  double g_sigma_p = 0;      // 1 / 3.0f;
-  double g_sigma_v = 0;      // sqrt(2)*g_sigma_p/(g_output_rate_divider/1000) / 3.0f;
-  double g_sigma_phi = 0;    // M_PI / 180.0f * 1 / 3.0f;
-  double g_sigma_omega = 0;  // sqrt(2)*g_sigma_phi/(g_output_rate_divider/1000) / 3.0f;
-  // https://stackoverflow.com/questions/25193991/how-to-initialize-boostmt19937-with-multiple-values-without-using-c11
-  boost::random::seed_seq seed_x({1ul, 2ul, 3ul, 4ul});
-  boost::random::seed_seq seed_y({5ul, 6ul, 7ul, 8ul});
-  boost::random::seed_seq seed_z({9ul, 10ul, 11ul, 12ul});
-  boost::random::seed_seq seed_v_x({13ul, 14ul, 15ul, 16ul});
-  boost::random::seed_seq seed_v_y({17ul, 18ul, 19ul, 20ul});
-  boost::random::seed_seq seed_v_z({21ul, 22ul, 23ul, 24ul});
-  boost::random::seed_seq seed_roll({25ul, 26ul, 27ul, 28ul});
-  boost::random::seed_seq seed_pitch({29ul, 30ul, 31ul, 32ul});
-  boost::random::seed_seq seed_yaw({33ul, 34ul, 35ul, 36ul});
-  boost::random::seed_seq seed_om_x({37ul, 38ul, 39ul, 40ul});
-  boost::random::seed_seq seed_om_y({41ul, 42ul, 43ul, 44ul});
-  boost::random::seed_seq seed_om_z({45ul, 46ul, 47ul, 48ul});
-
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_x(boost::mt19937(seed_x), boost::normal_distribution<>(0, g_sigma_p));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_y(boost::mt19937(seed_y), boost::normal_distribution<>(0, g_sigma_p));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_z(boost::mt19937(seed_z), boost::normal_distribution<>(0, g_sigma_p));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_v_x(boost::mt19937(seed_v_x), boost::normal_distribution<>(0, g_sigma_v));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_v_y(boost::mt19937(seed_v_y), boost::normal_distribution<>(0, g_sigma_v));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_v_z(boost::mt19937(seed_v_z), boost::normal_distribution<>(0, g_sigma_v));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_roll(boost::mt19937(seed_roll), boost::normal_distribution<>(0, g_sigma_phi));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_pitch(boost::mt19937(seed_pitch), boost::normal_distribution<>(0, g_sigma_phi));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_yaw(boost::mt19937(seed_yaw), boost::normal_distribution<>(0, g_sigma_phi));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_om_x(boost::mt19937(seed_om_x), boost::normal_distribution<>(0, g_sigma_omega));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_om_y(boost::mt19937(seed_om_y), boost::normal_distribution<>(0, g_sigma_omega));
-  boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
-      g_noise_generator_om_z(boost::mt19937(seed_om_z), boost::normal_distribution<>(0, g_sigma_omega));
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_x_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_y_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_z_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_v_x_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_v_y_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_v_z_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_roll_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_pitch_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_yaw_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_om_x_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_om_y_;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<>> noise_generator_om_z_;
 };
 }  // namespace gazebo
 
